1e73f1f0736440288f055472a42017219ca87ba8
wei4r <hankwei02151@gmail.com>
2024-07-06 15:29:32 +0800

    Add page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Game from "../components/game";
 import styles from "../css/page.module.css";
 import { useDetectDevice } from '../hooks/useDetectDevice';
@@ -7,16 +8,21 @@ import MobileNotSupported from '../components/MobileNotSupported';
 export default function App() {
   const isMobile = useDetectDevice(); // Move this line inside the App component
   return (
-    <main className={styles.main}>
-      {isMobile ? (
-        <MobileNotSupported />
-      ) : (
-        <>
-        <Game></Game>
-        </>
-        
-      )}      
+    <>
+      <Head>
+        <title>wei4r.type</title>
+      </Head>
+      <main className={styles.main}>
+        {isMobile ? (
+          <MobileNotSupported />
+        ) : (
+          <>
+          <Game></Game>
+          </>
+          
+        )}      
 
-    </main>
+      </main>
+    </>
   );
 }
